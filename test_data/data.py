2814f1785d03e52a17cf8ac9fbe502db191ec2e5
hitesh-qa <hitesh33733@gmail.com>
2026-05-07 16:15:37 +0530

    Add test data config with base URL, credentials, browser setting and expected messages

---
+++ test_data/data.py
@@ -0,0 +1,27 @@
+#----------- Base Url -------------
+BASE_URL = "https://www.saucedemo.com"
+
+#---------------Browser Settings----------
+BROWSER = "chromium"
+HEADLESS = False
+
+#---------Valid Login Credentials-------------
+VALID_USERNAME = "standard_user"
+VALID_PASSWORD = "secret_sauce"
+
+#------------Invalid Login Credentials-------------
+INVALID_USERNAME ="wrong_user"
+INVALID_PASSWORD = "wrong_password"
+
+#--------------Locked Out User-------------
+LOCKED_USER = "locked_out_user"
+
+#-----------Product Data-----------
+PRODUCT_NAME = "Sauce Labs Backpack"
+PRODUCT_PRICE = "$29.99"
+
+#--------Expected Message-------------------
+LOGIN_ERROR_MSG = "Epic sadface: Username and password do not match any user in this service"
+LOCKED_ERROR_MSG = "Epic sadface: Sorry, this user has been locked out"
+CART_PAGE_TITLE = "Your Cart"
+CHECKOUT_TITLE = "Checkout: Your Information"
