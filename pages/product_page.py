e13053be6f8a46a87d16e39f02c5f507146f7637
hitesh-qa <hitesh33733@gmail.com>
2026-05-16 15:49:30 +0530

    Writing action funtion in product_page.py

--- pages/product_page.py
+++ pages/product_page.py
@@ -13,3 +13,28 @@ REMOVE_BUTTON = "button[data-test^='remove']"
 BACK_BUTTON = "#back-to-products"
 
 #------------Actions-------------
+def get_product_name(self):
+    """Return product name text."""
+    return self.get_text(self.PRODUCT_NAME)
+
+def get_product_price(self):
+    """Return product price text."""
+    return self.get_text(self.PRODUCT_PRICE)
+
+def get_product_description(self):
+    """Return product description text."""
+    return self.get_text(self.PRODUCT_DESC)
+
+def add_to_cart(self):
+    """Click Add to cart button."""
+    self.click(self.ADD_TO_CART_BUTTON)
+
+def remove_from_cart(self):
+    """Click Remove button."""
+    self.click(self.REMOVE_BUTTON)
+
+def go_back(self):
+    """Click back to products button."""
+    self.click(self.BACK_BUTTON)
+
+#-------------Assertions---------
