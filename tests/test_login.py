9b69524b052e7110807dbc1365ebbcd59af16f46
hitesh-qa <hitesh33733@gmail.com>
2026-06-15 21:16:19 +0530

    add smoke tests in login test page

--- tests/test_login.py
+++ tests/test_login.py
@@ -17,3 +17,27 @@ def test_valid_login(self, login_page):
     login_page.login(VALID_USERNAME, VALID_PASSWORD)
     assert login_page.is_login_successful(), \
     "Login failed - inventory page not loaded"
+
+@pytest.mark.smoke
+@pytest.mark.login
+def test_invalid_login(self, login_page):
+    """Test login with invalid credentials shows error."""
+    login_page.open()
+    login_page.login(INVALID_USERNAME, INVALID_PASSWORD)
+    assert login_page.is_error_visible(), \
+        "Error message not displayed"
+    assert LOGIN_ERROR_MSG in login_page.get_error_message(), \
+        "Wrong error message displayed"
+
+#--------------Regression Tests---------------
+
+@pytest.mark.regression
+@pytest.mark.login
+def test_locked_out_user(self, login_page):
+    """Test locked out user sees correct error."""
+    login_page.open()
+    login_page.login(LOCKED_USER, VALID_PASSWORD)
+    assert login_page.is_error_visible(), \
+        "Error message not displayed for locked user."
+    assert LOCKED_ERROR_MSG in login_page.get_error_message(), \
+        "Wrong error message for locked user."
