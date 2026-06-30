260a54882202bfc8ce314ae815d3c29f2ba96be5
hitesh-qa <hitesh33733@gmail.com>
2026-06-30 08:40:44 +0530

    add smoke and regression test in main.py

--- main.py
+++ main.py
@@ -11,3 +11,26 @@ def run_all_test()
         "--self-contained-html",
         "-v"
     ])
+
+def run_smoke_test():
+    """Run only smoke tests."""
+    print("\n Running smoke tests...\n")
+    subprocess.run([
+        "test/",
+        "-m", "smoke"
+        "--html=reports/smoke_report.html",
+        "--self-contained-html"
+        "-v"
+    ])
+
+def run_regression_test():
+    """Run only regression tests."""
+    print("\n Running REGRESSION tests...\n")
+    subprocess.run([
+        sys.executable, "-m", "pytest",
+        "test/",
+        "-m", "regression",
+        "--html=reports/regression_report.html",
+        "-v"
+    ])
+
